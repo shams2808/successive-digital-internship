48df6764fdb1bea53d78aded15f0a8d5fca2900f
shams2808 <shamitjain.28@gmail.com>
2026-04-30 11:29:38 +0530

    Day 4 Task

--- Week 1/Day_3/counter_factory.js
+++ Week 1/Day_3/counter_factory.js
@@ -11,7 +11,7 @@ c1 = counterFactory();
 c2 = c1;                    //c1 and c2 shares same memory
 c3 = counterFactory();      //c3 has different memory
 
-c1();
-c2();
-c1();
-c3();
+c1();                       //1, increements and prints
+c2();                       //2, c2 and c1 shares same memory so increements and prints
+c1();                       //3, again updates the value saved by c2 and prints 3
+c3();                       //1, c3 shares different memory so updates it from 0 to 1 and prints it
---
+++ Week 1/Day_4/object.js
@@ -0,0 +1,11 @@
+let student = {
+    name: "Shamit",
+    age: 21,
+    city: "Ghazabad",
+    isGraduated: false,
+}
+
+console.log(student.name);          //Shamit
+console.log(student["age"])
+console.log(Object.keys(student))
+console.log(Object.values(student))
